b7a2fe50c0d2ffc261f96add64bfc49c0e0de3ba
mdtofazzalislam <dev.tofazzal122@gmail.com>
2025-05-07 12:24:48 +0600

    different way to rendering explore

--- src/Todo.jsx
+++ src/Todo.jsx
@@ -1,4 +1,4 @@
-export default function Todo({learn,isDo}){
+// export default function Todo({learn,isDo}){
   
     if(isDo){
         return(
@@ -7,4 +7,46 @@ export default function Todo({learn,isDo}){
     }
     return <li>Have to learn: {learn}</li>
 }
+
+
+//option: ternary condition 
+export default function Todo({learn,isDo}){
+    return(
+        <li>{isDo?"finished" : "work on"} : {learn}</li>
+    )
+}
+
+
+//condition && operator use
+export default function Todo({learn,isDo}){
+    return(
+        <li>{learn} : {isDo && "Already Done"}</li>
+    )
+}
+
+//condition || operator use
+export default function Todo({learn,isDo}){
+    return(
+        <li>{learn} : {isDo || "Have to learn"}</li>
+    )
+}
+
+// variable set rendering 
+
+export default function Todo({learn,isDo}){
+    let listItem;
+   
+    if(isDo){
+        <li>finished : {learn}</li>
+    }
+    else{
+        <li>Have to learn: {learn}</li>
+    }
+
+    return listItem;
+       
+    
+}
+
+
 
