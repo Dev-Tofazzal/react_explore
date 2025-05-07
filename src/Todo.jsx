ed5238b0848cf0b24ca2194e5d791ce180687dd4
mdtofazzalislam <dev.tofazzal122@gmail.com>
2025-05-07 12:27:23 +0600

    some correction

--- src/Todo.jsx
+++ src/Todo.jsx
@@ -1,35 +1,35 @@
 // export default function Todo({learn,isDo}){
   
-    if(isDo){
-        return(
-             <li>Finished: {learn}</li>
-        )
-    }
-    return <li>Have to learn: {learn}</li>
-}
+//     if(isDo){
+//         return(
+//              <li>Finished: {learn}</li>
+//         )
+//     }
+//     return <li>Have to learn: {learn}</li>
+// }
 
 
 //option: ternary condition 
-export default function Todo({learn,isDo}){
-    return(
-        <li>{isDo?"finished" : "work on"} : {learn}</li>
-    )
-}
+// export default function Todo({learn,isDo}){
+//     return(
+//         <li>{isDo?"finished" : "work on"} : {learn}</li>
+//     )
+// }
 
 
 //condition && operator use
-export default function Todo({learn,isDo}){
-    return(
-        <li>{learn} : {isDo && "Already Done"}</li>
-    )
-}
+// export default function Todo({learn,isDo}){
+//     return(
+//         <li>{learn} : {isDo && "Already Done"}</li>
+//     )
+// }
 
 //condition || operator use
-export default function Todo({learn,isDo}){
-    return(
-        <li>{learn} : {isDo || "Have to learn"}</li>
-    )
-}
+// export default function Todo({learn,isDo}){
+//     return(
+//         <li>{learn} : {isDo || "Have to learn"}</li>
+//     )
+// }
 
 // variable set rendering 
 
@@ -37,10 +37,10 @@ export default function Todo({learn,isDo}){
     let listItem;
    
     if(isDo){
-        <li>finished : {learn}</li>
+       listItem =  <li>finished : {learn}</li>
     }
     else{
-        <li>Have to learn: {learn}</li>
+       listItem =  <li>Have to learn: {learn}</li>
     }
 
     return listItem;
